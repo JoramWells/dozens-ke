8130bce3bb2127113dead3e2c1f3a1da5126b674
unknown <jorammanoah1@gmail.com>
2021-06-03 01:55:55 -0700

    Added cursor pointer

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Head from 'next/head'
+import Head from "next/head";
 import {
   CashIcon,
   GiftIcon,
@@ -10,13 +10,12 @@ import {
 export default function HomePage() {
   return (
     <>
-    <Head>
+      <Head>
         <title>HomePage | Dozens Kenya</title>
-        
-    </Head>
+      </Head>
       <main className="p-8">
         <div className=" flex flex-row justify-between content-center items-center mt-8">
-          <div className="p-2 hover:shadow-md flex flex-col content-center items-center">
+          <div className="p-2 hover:shadow-md flex flex-col content-center items-center hover:cursor-pointer">
             <div>
               <SwitchVerticalIcon className="h-10 w-10  text-red-500 bg-red-300 rounded-full p-1 shadow-xl " />
             </div>
@@ -30,7 +29,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="p-2 hover:shadow-md flex flex-col items-center content-center">
+          <div className="p-2 hover:shadow-md flex flex-col items-center content-center hover:cursor-pointer">
             <div>
               <TruckIcon className="h-10 w-10 text-blue-500 rounded-full bg-blue-300 p-1 shadow-xl" />
             </div>
@@ -44,7 +43,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="p-2 hover:shadow-md flex flex-col content-center items-center">
+          <div className="p-2 hover:shadow-md flex flex-col content-center items-center hover:cursor-pointer">
             <div>
               <CashIcon className="h-10 w-10  text-green-500 bg-green-300 rounded-full p-1 shadow-xl " />
             </div>
@@ -58,7 +57,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="p-2 hover:shadow-md flex-col flex content-center items-center">
+          <div className="p-2 hover:shadow-md flex-col flex content-center items-center hover:cursor-pointer">
             <div>
               <GiftIcon className="h-10 w-10 text-yellow-500 rounded-full bg-yellow-300 p-1 shadow-xl" />
             </div>
@@ -72,6 +71,12 @@ export default function HomePage() {
               </p>
             </div>
           </div>
+        </div>
+        <div className="p-2 mt-8 bg-yellow-300 rounded-md">
+            <p className="font-semibold text-gray-700 text-lg hover:underline hover:text-blue-500 hover:cursor-pointer">
+            This week offers
+
+            </p>
         </div>
       </main>
     </>
