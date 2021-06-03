eddc87b70bb3e73ad4f554da7709899331100747
unknown <jorammanoah1@gmail.com>
2021-06-03 10:28:42 -0700

    Addded react-multi carousel

--- components/HomePage.js
+++ components/HomePage.js
@@ -15,6 +15,27 @@ import {
   DotsCircleHorizontalIcon,
   ShoppingBagIcon,
 } from "@heroicons/react/outline";
+import Carousel from "react-multi-carousel";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export default function HomePage() {
   return (
@@ -22,9 +43,9 @@ export default function HomePage() {
       <Head>
         <title>HomePage | Dozens Kenya</title>
       </Head>
-      <main className="p-8">
+      <main className="p-8 bg-gray-50">
         <div className="flex flex-row space-x-8 justify-center content-center items-center">
-          <div className="ring-1 ring-gray-100 rounded-md p-2">
+          <div className="ring-1 ring-gray-100 rounded-md p-2 bg-white">
             <ul className="text-xs text-gray-500 leading-8 items-center content-center">
               <li className="flex flex-row space-x-2">
                 <DeviceMobileIcon className="h-5 mt-1.5" />
@@ -67,8 +88,8 @@ export default function HomePage() {
         </div>
 
         {/* Header */}
-        <div className=" flex flex-row justify-center content-center items-center space-x-14 mt-14">
-          <div className="px-8 py-2 rounded-md shadow-sm  flex flex-row content-center items-center cursor-pointer ">
+        <div className=" flex flex-row justify-center content-center items-center space-x-14 mt-14 mb-14">
+          <div className="px-8 py-2 rounded-md shadow-sm bg-white flex flex-row content-center items-center cursor-pointer ">
             <div>
               <SwitchVerticalIcon className="h-10 w-10 space-x-2 text-red-500 bg-red-300 rounded-full p-2  shadow-xl " />
             </div>
@@ -78,7 +99,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="px-8 py-2 shadow-sm rounded-md flex flex-row items-center content-center cursor-pointer">
+          <div className="px-8 py-2 shadow-sm rounded-md flex bg-white flex-row items-center content-center cursor-pointer">
             <div>
               <TruckIcon className="h-10 w-10 mr-1 text-blue-500 space-x-4 rounded-full bg-blue-300 p-2 shadow-xl" />
             </div>
@@ -88,7 +109,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="px-8 py-2 space-x-2 shadow-sm rounded-md flex flex-row items-center content-center cursor-pointer">
+          <div className="px-8 bg-white py-2 space-x-2 shadow-sm rounded-md flex flex-row items-center content-center cursor-pointer">
             <div>
               <CashIcon className="h-10 w-10  text-green-500 bg-green-300 rounded-full p-2 shadow-xl " />
             </div>
@@ -98,7 +119,7 @@ export default function HomePage() {
               </p>
             </div>
           </div>
-          <div className="px-8 py-2 shadow-sm rounded-md flex flex-row items-center content-center space-x-2 cursor-pointer">
+          <div className="px-8 py-2 bg-white shadow-sm rounded-md flex flex-row items-center content-center space-x-2 cursor-pointer">
             <div>
               <GiftIcon className="h-10 w-10 text-yellow-500 rounded-full bg-yellow-300 p-2 shadow-xl" />
             </div>
@@ -109,12 +130,77 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        <div className="" style={{ height: "400px" }}>
-          {/* <div className="ring-1 p-2 mt-8  rounded-md ring-gray-400">
-            <p className="font-semibold text-gray-700 text-lg  hover:cursor-pointer">
-              This week offers
-            </p>
-          </div> */}
+        <div
+          className="mt-8 bg-white p-2 rounded-md"
+          style={{
+            height: "400px",
+            margin: "auto",
+            display: "block",
+            width: "85%",
+          }}
+        >
+          <Carousel responsive={responsive}>
+            <figure className="p-0  m-1 hover:shadow-md cursor-pointer ">
+              <Image
+                src="https://images.unsplash.com/photo-1511556532299-8f662fc26c06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                height="160"
+                width="250"
+                alt="Sneakers image"
+              />
+              <div className="p-2">
+                <blockquote>
+                  <p className="text-gray-400 text-md">Nick Air</p>
+                  <p className="font-semibold text-gray-500">Ksh 2,341 /=</p>
+                  <p className="line-through text-gray-300">Ksh 3,457</p>
+                </blockquote>
+              </div>
+            </figure>
+            <figure className="p-0  m-1 hover:shadow-md cursor-pointer ">
+              <Image
+                src="https://images.unsplash.com/photo-1508125673219-7cec6bc90159?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"
+                height="160"
+                width="250"
+                alt="Sneakers image"
+              />
+              <div className="p-2">
+                <blockquote>
+                  <p className="text-gray-400 text-md">Nick Air</p>
+                  <p className="font-semibold text-gray-500">Ksh 2,341 /=</p>
+                  <p className="line-through text-gray-300">Ksh 3,457</p>
+                </blockquote>
+              </div>
+            </figure>
+            <figure className="p-0  m-1 hover:shadow-md cursor-pointer ">
+              <Image
+                src="https://images.unsplash.com/photo-1618329254028-833d820b7f86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+                height="160"
+                width="250"
+                alt="Sneakers image"
+              />
+              <div className="p-2">
+                <blockquote>
+                  <p className="text-gray-400 text-md">Nick Air</p>
+                  <p className="font-semibold text-gray-500">Ksh 2,341 /=</p>
+                  <p className="line-through text-gray-300">Ksh 3,457</p>
+                </blockquote>
+              </div>
+            </figure>
+            <figure className="p-0  m-1 hover:shadow-md cursor-pointer ">
+              <Image
+                src="https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=758&q=80"
+                height="160"
+                width="250"
+                alt="Sneakers image"
+              />
+              <div className="p-2">
+                <blockquote>
+                  <p className="text-gray-400 text-md">Nick Air</p>
+                  <p className="font-semibold text-gray-500">Ksh 2,341 /=</p>
+                  <p className="line-through text-gray-300">Ksh 3,457</p>
+                </blockquote>
+              </div>
+            </figure>
+          </Carousel>
         </div>
       </main>
     </>
