360935d3a305c703cd2f72a07c041dfb268aaa1c
unknown <jorammanoah1@gmail.com>
2021-06-03 01:13:46 -0700

    Added navbar

---
+++ components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <div>
+            <nav className="flex flex-row shadow-xl justify-between p-4 content-center items-center">
+                <div className="">
+                    Home
+                </div>
+                <div className="">
+                    Search
+                </div>
+                <div>
+                    Register
+                </div>
+            </nav>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Head from 'next/head'
+import Navigation from "../components/Navigation";
 
 export default function Home() {
   return (
-    <div >
-      <Head>
-        <title>Homepage | Dozens Kenya</title>
-        <meta name="description" content="Lets buy cheap" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <div className="font-extrabold text-xl">
-          Hello Kenya
-        </div>
-      </main>
-    </div>
-  )
+    <>
+      <Navigation />
+    </>
+  );
 }
